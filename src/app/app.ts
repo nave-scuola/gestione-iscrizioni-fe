9824cf5d4c3d4db81bd15c7a92406fc61ca31a16
fabripis <fpistis05@gmail.com>
2026-05-27 17:46:07 +0200

    Completato task FE-2.02 e testato con successo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Card } from './shared/components/card/card';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title = 'gestione-iscrizioni-fe';
+  title = 'Gestione Iscrizioni';
+
+  corsi = [
+    {id: 1, titolo: 'Corso Angular', descrizione: 'Corso per test', image: 'https://js.devexpress.com/Content/Images/Frameworks/Angular.png'},
+    {id: 2, titolo: 'Corso Java', descrizione: 'Corso per test', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnM3JO-10pqUHbWWP5tuOo-9LKUZhogZEEww&s'},
+    {id: 3, titolo: 'Corso HTML', descrizione: 'Corso per test', image: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'},
+  ];
 }
