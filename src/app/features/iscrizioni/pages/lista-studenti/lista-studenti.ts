186db64bc9353fe65927e43bc413d8e539b3badc
fabripis <fpistis05@gmail.com>
2026-05-29 13:00:27 +0200

    Concluso task FE-3.01 e testato senza errori di compilazione da parte di TypeScript

--- src/app/features/iscrizioni/pages/lista-studenti/lista-studenti.ts
+++ src/app/features/iscrizioni/pages/lista-studenti/lista-studenti.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Card } from '../../../../shared/components/card/card';
+import { StudenteRTO } from '../../models/studente.model';
 
 @Component({
   selector: 'app-lista-studenti',
@@ -10,4 +11,10 @@ import { Card } from '../../../../shared/components/card/card';
 })
 export class ListaStudenti {
   titolo = 'Gestione Iscrizioni';
+
+  readonly studenti: StudenteRTO[] = [
+    {idStudente: 1, nome: 'Mario', cognome: 'Rossi', email: 'mario.rossi@email.it', citta: 'Napoli', cf: 'MRGSFE85D67V480F', codiceStudente: 'STU-001'},
+    {idStudente: 2, nome: 'Luigi', cognome: 'Verdi', email: 'luigi.verdi@email.it', citta: 'Milano', cf: 'LGVRDE85D67U789G', codiceStudente: 'STU-002'},
+    {idStudente: 3, nome: 'Laura', cognome: 'Neri', email: 'laura.neri@email.it', citta: 'Molise', cf: 'LRNRIE89F67V998V', codiceStudente: 'STU-003'},
+  ]
 }
